26ab0cfc3b7f5579be7096ba48e634305e265317
John Mercouris <john@mercouris.email>
2018-06-13 17:38:50 +0200

    using render-page instead of embedding all cl-markup in the top level

--- src/view.lisp
+++ src/view.lisp
@@ -16,4 +16,10 @@
   (setf (getf (response-headers *response*) :content-type) "application/json")
   (encode-json object))
 
-(defun render-page (object))
+(defun render-page (object)
+  (cl-markup:html
+   (:body
+    (:meta :name "viewport" :content "width=device-width, initial-scale=1")
+    (:link :rel "stylesheet" :type "text/css" :media "screen" :href "/css/pure-min.css")
+    (:link :rel "stylesheet" :type "text/css" :media "screen" :href "/css/main.css")
+    (cl-markup:raw object))))
--- src/web.lisp
+++ src/web.lisp
@@ -21,13 +21,26 @@
 (clear-routing-rules *web*)
 
 (defroute "/" ()
-  (cl-markup:html
-   (:body
-    (:meta :name "viewport" :content "width=device-width, initial-scale=1")
-    (:link :rel "stylesheet" :type "text/css" :media "screen" :href "/css/pure-min.css")
-    (:link :rel "stylesheet" :type "text/css" :media "screen" :href "/css/main.css")
-    (:h1 "Hello World."))))
+  (render-page
+   (cl-markup:markup (:h1 "Hello Worlds."))))
 
+(defroute "/login" (&key _parsed)
+  (render-page
+   (cl-markup:markup
+    (:h1 "Login")
+    (:form :class "pure-form" :action "/login"
+           (:p "Username")
+           (:input :type "text" :name "login[username]")
+           (:p "Password")
+           (:input :type "text" :name "login[password]")
+           (:br)
+           (:br)
+           (:button :type "submit" :class "pure-button" "Submit")))))
+
+(defroute "/logout" ()
+  (render-page
+   (cl-markup:markup
+    (:h1 "Logged Out."))))
 
 ;; Error pages
 (defmethod on-exception ((app <web>) (code (eql 404)))
