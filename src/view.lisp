2452cc027700e1f2c50f197d35d347a9196e34ad
John Mercouris <john@mercouris.email>
2018-06-20 18:02:03 +0200

    add menu

--- src/view.lisp
+++ src/view.lisp
@@ -12,6 +12,23 @@
            :render-json))
 (in-package :source.view)
 
+(defun render-menu ()
+  (cl-markup:markup
+   (:div :class "pure-menu pure-menu-horizontal"
+         (:ul :class "pure-menu-list"
+              (:li :class "pure-menu-item"
+                   (:a :href "/" :class "pure-menu-link"
+                       "Index"))
+              (:li :class "pure-menu-item"
+                   (:a :href "/create/repository" :class "pure-menu-link"
+                       "Create Repository"))
+              (:li :class "pure-menu-item"
+                   (:a :href "/login" :class "pure-menu-link"
+                       "Login"))
+              (:li :class "pure-menu-item"
+                   (:a :href "/logout" :class "pure-menu-link"
+                       "Logout"))))))
+
 (defun render-json (object)
   (setf (getf (response-headers *response*) :content-type) "application/json")
   (encode-json object))
@@ -22,4 +39,5 @@
     (:meta :name "viewport" :content "width=device-width, initial-scale=1")
     (:link :rel "stylesheet" :type "text/css" :media "screen" :href "/css/pure-min.css")
     (:link :rel "stylesheet" :type "text/css" :media "screen" :href "/css/main.css")
+    (cl-markup:raw (render-menu))
     (cl-markup:raw object))))
